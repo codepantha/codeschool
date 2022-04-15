811670ea7717c6004a074b669ce553b462ad8682
codepanther <pezechigozirim@gmail.com>
2022-04-15 11:04:57 +0100

    extract a primaryGradient custom tailwind class

---
+++ pages/register.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const register = () => {
+  return (
+    <header className="h-28 primaryGradient flex flex-col justify-center items-center">
+      <h1 className="text-white text-2xl">
+        Register
+      </h1>
+    </header>
+  )
+}
+
+export default register
