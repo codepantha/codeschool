f50cc337d82e5d67164196c06fc4e561d1efe9ca
codepanther <pezechigozirim@gmail.com>
2022-04-15 09:53:05 +0100

    add antd and set up tailwindcss

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "bc976a25f2950319"; }
+/******/ 		__webpack_require__.h = function() { return "12275d9f71e9d2a5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
 const Index = () => {
   return (
     <>
-      Welcome to Next.js!
+      <header className="h-28 bg-gradient-to-r from-primaryGradient to-black flex flex-col justify-center items-center">
+        <h1 className="text-white text-2xl">
+          Welcome to CodeSchool
+        </h1>
+      </header>
     </>
   )
 }
